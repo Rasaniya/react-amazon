26cf83d0b106a80adddb66315bddba963b81fe5c
Rasaniya <rasaniyasoham1988@gmail.com>
2021-05-22 11:29:44 +0530

    Completed Header, Style Remaining

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './style.css';
+import Header from './Header.js';
 
 export default function App() {
   return (
     <div className="app">
       <h1>Hello Amazon!</h1>
+      <Header />
+      {/* Header */}
+      {/* Home */}
     </div>
   );
 }
