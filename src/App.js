10362aa86dff2370583ee0280abf59e2560c91fb
Rasaniya <rasaniyasoham1988@gmail.com>
2021-05-22 11:54:45 +0530

    Added Search Icon and styled Header Component and amazon logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header.js';
 export default function App() {
   return (
     <div className="app">
-      <h1>Hello Amazon!</h1>
+      {/* <h1>Hello Amazon!</h1> */}
       <Header />
       {/* Header */}
       {/* Home */}
