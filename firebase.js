6488d376fba6fb4b0c18fe0f952b5de048ea5ab4
Rasaniya <rasaniyasoham1988@gmail.com>
2021-05-22 10:50:21 +0530

    added firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCMv3ZzM03fLkQV0WB-JLqUmqBNTO1BdMc',
+  authDomain: 'react-50650.firebaseapp.com',
+  projectId: 'react-50650',
+  storageBucket: 'react-50650.appspot.com',
+  messagingSenderId: '272506189512',
+  appId: '1:272506189512:web:f74004c50f251070a257fd',
+  measurementId: 'G-VB9F3PPLM6'
+};
