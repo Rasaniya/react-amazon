04ededec31af0427a19f953a5264429051246677
Rasaniya <rasaniyasoham1988@gmail.com>
2021-05-23 11:13:05 +0530

    Created Product Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
 import Header from './Header.js';
+import Home from './Home.js';
 
 export default function App() {
   return (
     <div className="app">
       {/* <h1>Hello Amazon!</h1> */}
       <Header />
+      <Home />
       {/* Header */}
       {/* Home */}
     </div>
